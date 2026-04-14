b64c75496f83df53f4944fe3c8d2007db89c9b8f
sakshijha03 <93937944+sakshijha03@users.noreply.github.com>
2026-04-14 23:59:52 +0530

    Add files via upload

--- gretestPrioElement.py
+++ gretestPrioElement.py
@@ -10,8 +10,11 @@ Hence the output = 3.
 '''
 n=int(input())
 arr=list(map(int,input().split())) 
-count=0
-for i in range (len(arr)-1):
-    if arr[i]<arr[i+1]:
+count=1
+max_so_far=arr[0]
+for i in range (1,len(arr)):
+   
+    if arr[i]>max_so_far:
         count+=1
+        max_so_far=arr[i]
 print(count)
