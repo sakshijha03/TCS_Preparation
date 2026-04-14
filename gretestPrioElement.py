173c6a87328da08458700ae34a6a3371c499e937
sakshijha03 <93937944+sakshijha03@users.noreply.github.com>
2026-04-14 23:14:09 +0530

    Add files via upload

---
+++ gretestPrioElement.py
@@ -0,0 +1,17 @@
+'''  
+Given an integer array Arr of size N the task is to find the count of elements whose value is greater than all of its prior elements.
+Note : 1st element of the array should be considered in the count of the result.
+For example,
+Arr[]={7,4,8,2,9}
+As 7 is the first element, it will consider in the result.
+8 and 9 are also the elements that are greater than all of its previous elements.
+Since total of  3 elements is present in the array that meets the condition.
+Hence the output = 3.
+'''
+n=int(input())
+arr=list(map(int,input().split())) 
+count=0
+for i in range (len(arr)-1):
+    if arr[i]<arr[i+1]:
+        count+=1
+print(count)
